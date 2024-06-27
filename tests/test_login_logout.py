ff4f73bb9b358b05fe18736c414d9d1301000ae1
Anastasia Tsverova <ph.adobe@yandex.ru>
2024-06-27 21:58:56 +0300

    Refactoring according to the comment

--- tests/test_login_logout.py
+++ tests/test_login_logout.py
@@ -5,62 +5,62 @@ from test_utils import TestUtils as tu, MAIN_PAGE_URL
 
 class TestLoginLogout:
 
-    def test_log_in_login_button(self, driver_scope_module, login_data):
+    def test_log_in_login_button(self, driver, login_data):
         login_email, login_password = login_data
-        driver_scope_module.get(MAIN_PAGE_URL)
-        driver_scope_module.find_element(By.XPATH, L.LOG_IN_BUTTON).click()
-        tu.wait_for_loading(driver_scope_module, L.ENTRY_HEADER)
-        tu.check_log_in(driver_scope_module, login_email, login_password)
+        driver.get(MAIN_PAGE_URL)
+        driver.find_element(By.XPATH, L.LOG_IN_BUTTON).click()
+        tu.wait_for_loading(driver, L.ENTRY_HEADER)
+        tu.check_log_in(driver, login_email, login_password)
 
         assert (
-                "Оформить заказ" in driver_scope_module.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
+                "Оформить заказ" in driver.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
         )
-        tu.log_out(driver_scope_module)
+        tu.log_out(driver)
 
-    def test_log_in_my_account_button(self, driver_scope_module, login_data):
+    def test_log_in_my_account_button(self, driver, login_data):
         login_email, login_password = login_data
-        driver_scope_module.get(MAIN_PAGE_URL)
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        tu.wait_for_loading(driver_scope_module, L.ENTRY_HEADER)
-        tu.check_log_in(driver_scope_module, login_email, login_password)
+        driver.get(MAIN_PAGE_URL)
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        tu.wait_for_loading(driver, L.ENTRY_HEADER)
+        tu.check_log_in(driver, login_email, login_password)
         assert (
-                "Оформить заказ" in driver_scope_module.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
+                "Оформить заказ" in driver.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
         )
-        tu.log_out(driver_scope_module)
+        tu.log_out(driver)
 
-    def test_log_in_registration_form(self, driver_scope_module, login_data):
+    def test_log_in_registration_form(self, driver, login_data):
         login_email, login_password = login_data
-        driver_scope_module.get(MAIN_PAGE_URL)
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        tu.wait_for_loading(driver_scope_module, L.ENTRY_HEADER)
-        driver_scope_module.find_element(By.XPATH, L.REGISTRATION_LINK).click()
-        tu.wait_for_loading(driver_scope_module, L.REGISTRATION_HEADER)
-        driver_scope_module.find_element(By.XPATH, L.LOG_IN_LINK).click()
-        tu.wait_for_loading(driver_scope_module, L.ENTRY_HEADER)
-        tu.check_log_in(driver_scope_module, login_email, login_password)
+        driver.get(MAIN_PAGE_URL)
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        tu.wait_for_loading(driver, L.ENTRY_HEADER)
+        driver.find_element(By.XPATH, L.REGISTRATION_LINK).click()
+        tu.wait_for_loading(driver, L.REGISTRATION_HEADER)
+        driver.find_element(By.XPATH, L.LOG_IN_LINK).click()
+        tu.wait_for_loading(driver, L.ENTRY_HEADER)
+        tu.check_log_in(driver, login_email, login_password)
         assert (
-                "Оформить заказ" in driver_scope_module.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
+                "Оформить заказ" in driver.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
         )
-        tu.log_out(driver_scope_module)
+        tu.log_out(driver)
 
-    def test_log_in_recover_password_form(self, driver_scope_module, login_data):
+    def test_log_in_recover_password_form(self, driver, login_data):
         login_email, login_password = login_data
-        driver_scope_module.get(MAIN_PAGE_URL)
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        tu.wait_for_loading(driver_scope_module, L.ENTRY_HEADER)
-        driver_scope_module.find_element(By.XPATH, L.RECOVER_PASSWORD_LINK).click()
-        tu.wait_for_loading(driver_scope_module, L.PASSWORD_RECOVERY_HEADER)
-        driver_scope_module.find_element(By.XPATH, L.LOG_IN_LINK).click()
-        tu.wait_for_loading(driver_scope_module, L.ENTRY_HEADER)
-        tu.check_log_in(driver_scope_module, login_email, login_password)
+        driver.get(MAIN_PAGE_URL)
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        tu.wait_for_loading(driver, L.ENTRY_HEADER)
+        driver.find_element(By.XPATH, L.RECOVER_PASSWORD_LINK).click()
+        tu.wait_for_loading(driver, L.PASSWORD_RECOVERY_HEADER)
+        driver.find_element(By.XPATH, L.LOG_IN_LINK).click()
+        tu.wait_for_loading(driver, L.ENTRY_HEADER)
+        tu.check_log_in(driver, login_email, login_password)
         assert (
-                "Оформить заказ" in driver_scope_module.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
+                "Оформить заказ" in driver.find_element(By.XPATH, L.MAKE_ORDER_BUTTON).text
         )
-        tu.log_out(driver_scope_module)
+        tu.log_out(driver)
 
-    def test_log_out(self, driver_scope_module, login_data):
+    def test_log_out(self, driver, login_data):
         login_email, login_password = login_data
-        driver_scope_module.get(MAIN_PAGE_URL)
-        tu.log_in(driver_scope_module, login_email, login_password)
-        tu.log_out(driver_scope_module)
-        assert "/login" in driver_scope_module.current_url
+        driver.get(MAIN_PAGE_URL)
+        tu.log_in(driver, login_email, login_password)
+        tu.log_out(driver)
+        assert "/login" in driver.current_url
--- tests/test_my_account.py
+++ tests/test_my_account.py
@@ -14,14 +14,14 @@ class TestMyAccount:
             FEED_PAGE_URL,
         ],
     )
-    def test_go_to_my_account_from_different_pages_logged_in(self, driver_scope_module, path, login_data):
+    def test_go_to_my_account_from_different_pages_logged_in(self, driver, path, login_data):
         login_email, login_password = login_data
-        tu.log_in(driver_scope_module, login_email, login_password)
-        driver_scope_module.get(path)
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        tu.wait_for_loading(driver_scope_module, L.PROFILE_HEADER)
-        assert driver_scope_module.find_element(By.XPATH, L.PROFILE_USER_EMAIL)
-        tu.log_out(driver_scope_module)
+        tu.log_in(driver, login_email, login_password)
+        driver.get(path)
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        tu.wait_for_loading(driver, L.PROFILE_HEADER)
+        assert driver.find_element(By.XPATH, L.PROFILE_USER_EMAIL)
+        tu.log_out(driver)
 
     @pytest.mark.parametrize(
         "path",
@@ -30,10 +30,10 @@ class TestMyAccount:
             FEED_PAGE_URL,
         ],
     )
-    def test_go_to_my_account_from_different_pages_not_logged_in(self, driver_scope_module, path):
-        driver_scope_module.get(path)
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        assert driver_scope_module.find_element(By.XPATH, L.ENTRY_HEADER)
+    def test_go_to_my_account_from_different_pages_not_logged_in(self, driver, path):
+        driver.get(path)
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        assert driver.find_element(By.XPATH, L.ENTRY_HEADER)
 
     @pytest.mark.parametrize(
         "xpath",
@@ -42,14 +42,14 @@ class TestMyAccount:
             L.LOGO,
         ],
     )
-    def test_from_my_account_go_to_constructor_logged_in(self, driver_scope_module, xpath: str, login_data):
+    def test_from_my_account_go_to_constructor_logged_in(self, driver, xpath: str, login_data):
         login_email, login_password = login_data
-        tu.log_in(driver_scope_module, login_email, login_password)
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        tu.wait_for_loading(driver_scope_module, L.PROFILE_HEADER)
-        driver_scope_module.find_element(By.XPATH, xpath).click()
-        assert driver_scope_module.find_element(By.XPATH, L.MAKE_BURGER_HEADER)
-        tu.log_out(driver_scope_module)
+        tu.log_in(driver, login_email, login_password)
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        tu.wait_for_loading(driver, L.PROFILE_HEADER)
+        driver.find_element(By.XPATH, xpath).click()
+        assert driver.find_element(By.XPATH, L.MAKE_BURGER_HEADER)
+        tu.log_out(driver)
 
     @pytest.mark.parametrize(
         "xpath",
@@ -58,7 +58,7 @@ class TestMyAccount:
             L.LOGO,
         ],
     )
-    def test_from_my_account_go_to_constructor_not_logged_in(self, driver_scope_module, xpath: str):
-        driver_scope_module.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
-        driver_scope_module.find_element(By.XPATH, xpath).click()
-        assert driver_scope_module.find_element(By.XPATH, L.MAKE_BURGER_HEADER)
+    def test_from_my_account_go_to_constructor_not_logged_in(self, driver, xpath: str):
+        driver.find_element(By.XPATH, L.MY_ACCOUNT_BUTTON).click()
+        driver.find_element(By.XPATH, xpath).click()
+        assert driver.find_element(By.XPATH, L.MAKE_BURGER_HEADER)
